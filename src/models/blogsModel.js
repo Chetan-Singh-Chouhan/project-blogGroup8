eb7b077beb03f008f1e320ced0b3a9f95c5f5776
Chetan-Singh-Chouhan <mr.chiragchouhan@gmail.com>
2023-01-02 14:58:09 +0530

    creation of models

---
+++ src/models/blogsModel.js
@@ -0,0 +1,47 @@
+const mongoose =require('mongoose')
+const ObjectID= mongoose.Schema.Types.ObjectID
+const blogsSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required: true
+
+    },
+    body:{
+        type:String,
+        required: true
+    },
+    authorId:{
+        type: ObjectID,
+        ref: "Author",
+        required: true
+    },
+    tags:{
+        type:[String],
+        required: true
+    },
+    category:{
+        type:String,
+        required: true
+    },
+    subcategory:{
+        type : [String],
+       
+    },
+    deletedAt:
+    {
+        type:Date
+    },
+    publishedAt:{
+        type:Date
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    },
+    isPublished:{
+        type:Boolean,
+        default: false
+    }
+    
+},{timestamps:true})
+module.exports = mongoose.model("blog",blogsSchema)
---
+++ src/routes/route.js
@@ -0,0 +1,1 @@
+const authorController = require('../controller')
