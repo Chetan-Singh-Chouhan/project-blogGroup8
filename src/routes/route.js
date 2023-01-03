1442c47451f695adf1f5f27e01cd96e58216beab
Chetan-Singh-Chouhan <mr.chiragchouhan@gmail.com>
2023-01-03 18:13:26 +0530

    put api by chetan

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { Router } = require('express')
+//const { Router } = require('express')
 const authorController = require('../controller/authorController')
 const blogController = require("../controller/blogController")
 const middleware = require("../middleware/middleware")
+
 router.post("/createAuthor", authorController.createAuthor)
 router.post("/createblog",middleware.isValidAuthor,blogController.createblog)
 router.get("/getBlogs",blogController.getblog)
 router.delete("/blogs/:blogId",blogController.isdeletebyId)
+router.put("/blogs/:blogId",blogController.updateBlogData)
 
 
   
