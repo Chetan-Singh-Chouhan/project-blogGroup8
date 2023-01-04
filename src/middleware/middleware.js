c00e479f50efe2741935f0fef102abaecce8f0f9
Chetan-Singh-Chouhan <mr.chiragchouhan@gmail.com>
2023-01-04 10:59:23 +0530

    updated by chetan

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -2,11 +2,10 @@
 const authorModel = require("../models/authorModel")
 
 const isValidAuthor = async function(req,res,next){
-try{
-    let authorId = req.body.authorId
-    let finder =await authorModel.findById(authorId)
+try{let authorId = req.body.authorId
+let finder =await authorModel.findById(authorId)
 // console.log(finder)
-if(!finder) return res.status(404).send({error : "Author doenst exist"})
+if(!finder)return res.status(404).send({error : "Author doenst exist"})
 }
 catch{
     res.send({error : "Author doenst exist"})
