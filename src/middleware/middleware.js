9ce98490c00006a1f95ac6b068fd5388c066b207
Tirthankar Roy <roytirthankar2@gmail.com>
2023-01-04 12:53:35 +0530

    tirtha

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,8 +1,10 @@
 // const blogModel = require("../models/blogModel")
 const authorModel = require("../models/authorModel")
+const jwt = require("jsonwebtoken")
 
 const isValidAuthor = async function(req,res,next){
-try{let authorId = req.body.authorId
+try{
+    let authorId = req.body.authorId
 let finder =await authorModel.findById(authorId)
 // console.log(finder)
 if(!finder)return res.status(404).send({error : "Author doenst exist"})
@@ -12,4 +14,27 @@ catch{
 }  
 next()
 }
+
+const validToken = async function(req, res, next){
+    let token = req.headers["x-api-key"];
+    if (!token) return res.send({ status: false, msg: "Token must be present" });
+  
+    let decodedToken = jwt.verify(token, "group-8-project-secret-key");
+    if (!decodedToken)
+      return res.send({ status: false, msg: "Token is Invalid" });
+      next()
+}
+
+
+const loginUser = async function (req, res, next) {
+    let userName = req.body.emailId
+    let password = req.body.password
+    let user = await authorModel.findOne({ emailId: userName, password: password })
+    if (!user) {
+      return res.send({ status: false, msg: "Invalid User Name or Password" })
+    }
+    next()
+  }
 module.exports.isValidAuthor = isValidAuthor  
+module.exports.validToken = validToken
+module.exports.loginUser = loginUser
