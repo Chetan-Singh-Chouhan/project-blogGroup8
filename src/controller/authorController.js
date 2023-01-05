ce7d1ab0098a5bfe16c1b3cf0ab65f6e07edf666
Tirthankar Roy <roytirthankar2@gmail.com>
2023-01-05 12:56:41 +0530

    tirtha4

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -1,6 +1,7 @@
 const authorModel = require("../models/authorModel")
 const jwt = require("jsonwebtoken")
 const emailValidator = require("email-validator")
+let passwordValidator = require("password-validator")
 
 const createAuthor = async function(req, res){
     let data = req.body
@@ -12,13 +13,21 @@ const createAuthor = async function(req, res){
     if(!password) return res.status(400).send({status: false, msg:"password is required"})
 
     const validName = (/^[a-zA-Z_]{3,20}$/)
-    // const validPassword = (/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
 
-    if(!validName.test(fname)) return res.status(400).send({status: false, msg: "Invalid firstname"})
-    if(!validName.test(lname)) return res.status(400).send({status: false, msg: "Invalid lastname"})
+    
+    let schema = new passwordValidator();
+          schema
+                .is().min(8)  //Must be of minimum length 8
+                .is().max(100)  //Maximum length should not be more than 100
+                .has().uppercase()  //Must contain one uppercase
+                .has().lowercase()  //Must contain one lowercase
+                .has().digits(2)  //Must have atleast 2 digits
+
+    if(!validName.test(fname)) return res.status(400).send({status: false, msg: "Numbers Not Allowed & Must be of minimum 3 characters"})
+    if(!validName.test(lname)) return res.status(400).send({status: false, msg: "Numbers Not Allowed & Must be of minimum 3 characters"})
     if(!(["Mr", "Mrs", "Miss"].includes(title))) return res.status(400).send({status: false, msg: "Can only use Mr, Mrs and Miss"})
     if(!emailValidator.validate(email)) return res.status(400).send({status:false, msg:"Invalid Email"})
-    // if(!validPassword.test(password)) return res.status(400).send({status: false, msg: "Invalid password"})
+    if(!schema.validate(password)) return res.status(400).send({status: false, msg: "Password requirements didn't match"})
 
     let created = await authorModel.create(data)
     res.send({status: true, data: created})
